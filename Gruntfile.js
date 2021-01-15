45eebf0109dcfe45bfc4ae6cb7d6cd93350b87bb
Valeria <yangoler@gmail.com>
2021-01-15 17:08:15 +0200

    moved files to directories and some TEST changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,23 @@
+//Стандартный экспорт модуля в nodejs
 module.exports = function(grunt) {
-    // Do grunt-related things in here
+    // Инициализация конфига GruntJS
+    grunt.initConfig({
+
+        //Настройки различных модулей GruntJS, их нужно предварительно установить через менеджер пакетов npm, или добавить в файл package.json перед запуском npm install
+
+        //Например проверка кода javascript с помощью утилиты jshint
+        jshint: {},
+
+        //Склеивание файлов
+        concat: {}
+
+        //И так далее
+    });
+
+    //Загрузка модулей, которые предварительно установлены
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    //Эти задания будут выполнятся сразу же когда вы в консоли напечатание grunt, и нажмете Enter
+    grunt.registerTask('default', ['jshint', 'concat']);
 };

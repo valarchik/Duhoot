775f2988a6a33bcde07ae51130191c708a2ff1d8
Valeria <yangoler@gmail.com>
2021-01-23 17:55:00 +0200

    develop header: html,css,js

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,28 @@
-alert ('Hello Lera');
+let search = $('#search');
+let searchInput = $('#site-search');
+let searchIcon = document.querySelector(".search-icon");
+let searchIconQuery = $('.search-icon')
+
+searchIcon.addEventListener('click', function () {
+
+    if (!search.is(":visible") && !searchInput.is(":visible")) {
+        search.animate({width: "450px"}, 1000);
+        search.css('display', 'block');
+        searchInput.css('display', 'block');
+    }
+});
+
+$(document).mouseup(function (e) { // событие клика по веб-документу
+
+    if (!searchIconQuery.is(e.target) // если клик был не по нашему блоку
+        && searchIconQuery.has(e.target).length === 0 && search.is(":visible") && searchInput.is(":visible")) { // и не по его дочерним элементам
+        search.animate({width: "0px"}, 500);// скрываем его
+
+        setTimeout(function () {
+            search.css('display', 'none');
+            searchInput.css('display', 'none');
+        }, 480);
+    }
+});
+
+

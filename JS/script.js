3dae09bba9bcd04bd1be0f9826b01e318673c25b
Valeria <yangoler@gmail.com>
2021-02-01 16:27:14 +0200

    develop search form in header

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,11 @@
+//header
+
 let search = $('#search');
 let searchInput = $('#site-search');
 let searchIcon = document.querySelector(".search-icon");
 let searchIconQuery = $('.search-icon')
 
 searchIcon.addEventListener('click', function () {
-
     if (!search.is(":visible") && !searchInput.is(":visible")) {
         search.animate({width: "450px"}, 1000);
         search.css('display', 'block');
@@ -25,5 +26,66 @@ $(document).mouseup(function (e) { // событие клика по веб-до
         }, 480);
     }
 });
+
+//login-form
+
+let buttonLogin = document.querySelector(".login");
+let formLogin = $('.overlay');
+let closeButton = document.querySelector(".close-button");
+
+buttonLogin.addEventListener('click', function () {
+    formLogin.css('display', 'flex');
+});
+
+closeButton.addEventListener('click', function () {
+    if (formLogin.is(':visible')) {
+        formLogin.css('display', 'none');
+    }
+});
+
+//search-form
+
+let dropDown = document.querySelector(".dropdown-wr-country"),
+    select = $('.dropdown-country'),
+    countryItem = document.querySelectorAll('.country'),
+    selectedCountry = $('.selected-country');
+
+let dropDownAction = document.querySelector(".dropdown-wr-action"),
+    selectAction = $('.dropdown-action'),
+    countryItemAction = document.querySelectorAll('.action'),
+    selectedCountryAction = $('.selected-action');
+
+
+dropDown.addEventListener('click', function () {
+    if (!select.is(':visible')) {
+        select.css('display', 'block');
+    } else {
+        select.css('display', 'none');
+    }
+});
+
+for (let i = 0; i < countryItem.length; i++) {
+    countryItem[i].addEventListener('click', function (e) {
+        let valueCountry = $(countryItem[i]).html();
+        selectedCountry.text(valueCountry);
+    });
+}
+
+dropDownAction.addEventListener('click', function () {
+    if (!selectAction.is(':visible')) {
+        selectAction.css('display', 'block');
+    } else {
+        selectAction.css('display', 'none');
+    }
+});
+
+for (let i = 0; i < countryItemAction.length; i++) {
+    countryItemAction[i].addEventListener('click', function (e) {
+        let valueCountryAction = $(countryItemAction[i]).html();
+        selectedCountryAction.text(valueCountryAction);
+    });
+}
+
+
 
 
